018ea8373e15faf4fe7df922c6fa2d34af366ca1
hvijay-tata <99406041+hvijay-tata@users.noreply.github.com>
2022-04-13 13:05:17 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for TATA Cliq";
+        return "DevOps Project for TATA Cliq Demo";
     }
 }
